92aa1c7cf3cbfa2c15a0dcd09852ec72a3d63020
Ami9122699 <kizaki.job.20210729@gmail.com>
2023-12-05 14:29:46 +0900

    roomの登録機能を作成

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -1,4 +1,34 @@
 class RoomController < ApplicationController
-  def index
+
+    def index
+        @rooms = Room.all #allメソッドはテーブルのレコードを全て取得できるメソッド 
+    end #indexのend
+
+    def new
+        @room = Room.new
+    end #newのend
+      
+    def create
+        @room = Room.new(room_params)
+        if @room.save
+          flash[:notice] = "新しい予定を登録しました"  
+          redirect_to :room
+        else
+          flash[:notice] = "スケジュールの登録に失敗しました"
+          render "new"
+        end
+
+    end #createのend
+        
+    def show
+      @post = Room.find(params[:id])
+    end #showのend
+
+  
+
+  private
+  def room_params
+  room.require(:room).permit(:name, :introduction, :price, :address)
   end
+
 end
