40b0f9c826a3a50f854f55f1cbcbd21f242268f1
Ami9122699 <kizaki.job.20210729@gmail.com>
2024-02-22 10:57:24 +0900

    ActionController::ParameterMissing in ReservationsController#createエラーを修正中

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -58,7 +58,7 @@ class ReservationsController < ApplicationController
 
     private
     def reservation_params
-    params.require(:reservation).permit(:checkin, :checkout, :people, :room_id) 
+    params.require(:reservation).permit(:checkin, :checkout, :people, :room_id, :user_id) 
     end
     #room_idをストロングパラメーターに含める
 
